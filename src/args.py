6a6c7f4ccc3209a6d016db2b72fac8de48072dce
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-09-04 20:05:33 -0400

    added flag for demo

--- src/args.py
+++ src/args.py
@@ -4,8 +4,8 @@ def parse_api_run_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--expt_name", help="Experiment name in config file to read and operate", type=str)
     parser.add_argument("--save_folder_path", help="Folder Path to save All data to", type=str)
-    parser.add_argument("--openai_model_name", help="Name of the OpenAI model to use", default="gpt-3.5", 
-                        choices=["gpt-3.5", "gpt-4"], type=str)
+    parser.add_argument("--openai_model_name", help="Name of the OpenAI model to use", default="gpt-3.5-turbo", 
+                        choices=["gpt-3.5-turbo", "gpt-4"], type=str)
     parser.add_argument("--max_tokens", help="Max tokens to use for the model", default=350, type=int)
     # Add arg for max_replies for chat completion
     parser.add_argument("--cache_path", help="Path to save cache to", type=str)
@@ -16,4 +16,5 @@ def parse_api_run_args():
     parser.add_argument("--data_path", help="Path to load prompt data from", type=str)
     parser.add_argument("--api_key_name", help="API Key name to use from .api_key file. Defaults to default", 
                         default="default", type=str)
+    parser.add_argument("--demo", help="Run a demo of the experiment", action="store_true") 
     return parser.parse_args()
